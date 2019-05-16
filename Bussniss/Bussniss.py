65d83af6d9283d2e4241645092706f2605b6ed07
mr_zhai_sp@126.com <mr_zhai_sp@126.com>
2019-05-16 17:22:35 +0800

    sa

--- Bussniss/Bussniss.py
+++ Bussniss/Bussniss.py
@@ -1,2 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from Commonlib.Commonlib import Commonlib
+from Public.Public import PubLic
+from Commonlib.Readxml import Readxml
+from Commonlib.Loggin import LoggIn
+class Bussniss:
+
+    def __init__(self):
+        self.p = PubLic()
+        self.r = Readxml()
+        self.l = LoggIn()
+
+    def LoadVideo(self):
+        #登录上传视频后台系统
+        self.l.Logg("登录视频上传网站")
+        try:
+            self.p.login_load_video( self.r.readxml("baseurl","loadvideoUrl"),
+                                     self.r.readxml("loadvideologin","username"),
+                                     self.r.readxml("loadvideologin","password"))
+
+            self.p.pub.waite(1)
+            self.p.pub.activeEvent(".//*[contains(text(),'导入视频')]")
+            self.p.pub.waite(2)
+            #路径前面加上r处理路径下含U
+            self.l.Logg("上传视频文件")
+            self.p.pub.inputKeys(".//body/descendant::input[1]",r"C:\Users\Test\上传视频\人工智能的机器人.mp4")
+            self.p.pub.waite(2)
+            self.l.Logg("导入视频")
+            self.p.pub.activeEvent(".//body/descendant::button[6]")
+            # self.p.pub.waite(2)
+            # self.l.Logg("输入栏目为内网自编")
+            # self.p.pub.activeEvent(".//*[contains(text(),'内网自编')]")
+            # self.p.pub.waite(2)
+            # self.l.Logg("选择频道分类")
+            # self.p.pub.activeEvent(".//*[contains(text(),'频道分类')]/following::div[1]/descendant::input")
+
+        except Exception as e:
+            raise e
+
+b = Bussniss()
+b.LoadVideo()
+
+
--- Public/Public.py
+++ Public/Public.py
@@ -12,9 +12,9 @@ class PubLic:
     def __init__(self):
         self.pub = Commonlib()
 
-    def login_pub(self,username,password):
-        """封装登录模块"""
-        self.pub.openBrowser(r.readxml("baseurl","testurl"))
+    def login_ui_pub(self,base_url,username,password):
+        """封装UI登录模块"""
+        self.pub.openBrowser(base_url)
         self.pub.waite(1)
         self.pub.clearKeys(".//*[@class='el-input el-input-group el-input-group--prepend'][1]/input")
         self.pub.waite(1)
@@ -26,3 +26,30 @@ class PubLic:
         self.pub.waite(1)
         self.pub.activeEvent(".//*[contains(text(),'立即登录')]")
         self.pub.waite(2)
+
+    def login_load_video(self,base_url,username,password):
+        self.pub.openBrowser(base_url)
+        self.pub.waite(1)
+        self.pub.clearKeys(".//*[@class='el-input el-input-group el-input-group--prepend'][1]/input")
+        self.pub.waite(1)
+        self.pub.inputKeys(".//*[@class='el-input el-input-group el-input-group--prepend'][1]/input",username)
+        self.pub.waite(1)
+        self.pub.clearKeys(".//body/descendant::input[2]")
+        self.pub.waite(1)
+        self.pub.inputKeys(".//body/descendant::input[2]",password)
+        self.pub.waite(1)
+        self.pub.activeEvent(".//*[contains(text(),'立即登录')]")
+# b = PubLic()
+# b.login_load_video("http://10.168.103.151/admin/#/","admin","111111")
+
+
+
+
+
+
+
+
+
+
+
+
